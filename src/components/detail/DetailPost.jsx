58ed082e2561415eab7ec16bcb38f6403c76e067
gwonyeong <qwe980121@naver.com>
2022-08-20 12:27:14 +0900

    하경님 파일 추가

--- src/components/detail/DetailPost.jsx
+++ src/components/detail/DetailPost.jsx
@@ -1,9 +1,51 @@
-function DetailPost() {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-}
+import React from "react";
+import styled from "styled-components";
+import Layout from "../components/Layout";
+import { useNavigate, useParams } from "react-router-dom";
+import { FiArrowLeft, FiEdit, FiTrash2} from "react-icons/fi";
 
-export default DetailPost;
+const Detail = () => {
+ const navigate = useNavigate();
+  return (
+        <>
+        <Layout>
+        <Header>
+            <FiArrowLeft 
+                size={30}
+                onClick={()=>{
+                    navigate("/")
+                }}/>
+            <FiEdit 
+                size={30}
+                onClick={()=>{
+                    navigate("/edit/:editId")
+                }}/>
+            <FiTrash2 
+                size={30}
+                />
+            
+          {/* 이미지 */}
+        </Header>
+        {/* <Body>
+            <icon>프로필사진</icon>
+            <h3>큐컴버</h3>
+            <div>위치</div>
+            <hr />
+            <div>username</div>
+            <div>새상품입니다</div>
+        </Body>
+        <Bottom>
+            <div>하트</div>
+            <div>가격</div>
+            <button>채팅하기</button>
+        </Bottom> */}
+        </Layout>
+        </>
+)
+};
+export default Detail;
+const Header = styled.div`
+  height: 100px;
+  background-color: #ACD137;
+  color: white;
+ ` 
